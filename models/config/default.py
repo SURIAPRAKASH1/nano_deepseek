9fb1f0ad466afeaf78e48170bccf88bd6952a3a9
SURIAPRAKASH1 <suriaprakashsuri@gmail.com>
2025-05-20 04:40:15 -0700

    rewritting config

--- models/config/default.py
+++ models/config/default.py
@@ -1,27 +1,99 @@
 from dataclasses import dataclass
+from typing import Literal
 
-"default configuration for deepseek-v3. for quick training and see How model converges"
+" Default configuration for deepseek-v3. For quick training and see How model converges "
+
 @dataclass
-class GPTMOEConfig:
+class DeepSeekConfig:
 
-    n_embd: int = 128                # token embedding dim 
-    vocab_size: int = 102400         # number of unique tokens
-    n_layers: int = 4                 # num of layers
-    block_size: int = 16             # seq_len/context lenght what ever you name it
-    batch_size: int = 32             # How many sequence are packed togather ?
+    """
+    not here -> Deepseek-v3 didn't follow
 
-    # MLA - Multihead Latent Attention
-    ld: int = 8                          # fixed compressed dim for q, k, v to down-projection
-    n_heads: int = 4                      # number of heads
-    head_dim: int = n_embd // n_heads    # dim of each head 
+    n_embd (int): Embedding Dimention for each token.
+
+    vocab_size (int): total Number of unique tokens.
+    
+    n_layers (int): Number of layers in model.
+
+    block_size (int): Sequence length or Context size.
+
+    batch_size (int): How many sequence as a group ?.
+
+    inter_dim (int): Intermediate Dimentionality for MLP. (Usually 4 * n_embd not here).
+
+    n_dense_layers (int): How many Dense layer wanted to transformer have ?.
+
+    # MLA
+    q_lora_dim (int): Dimentionality for Low Rank Projection for query.
+
+    kv_lora_dim (int): Dimentionality for Low Rank Projection for key, value.
+
+    n_heads (int): Number of Heads in attention.
+
+    qk_nope_head_dim (int): Head Dim for query/key without positional embeddings. (Usually n_embd // n_heads not here).
+
+    qk_rope_head_dim (int): Head Dimentionality for q, k with positional embeddings.
+
+    v_head_dim (int): Head Dimentionality for v. (Usually n_embd // n_heads not here).
+    
+    # DeepSeekMOE
+    m (int): Fine-grained expert segmentation
+
+    num_experts (int): Total Number of Experts in MOE
+
+    n_shared_experts (int): Experts that are activated always. isolated shared experts
+
+    n_routing_experts (int): Number of experts that are activated sparsely
+
+    n_activated_experts (int): How many routing experts will be activated per token?
+
+    experts_dim (int): what's hidden state Dimentionality of Experts ? (Usually (4 * n_embd)//m not here)
+
+    score_func (Literal['sigmoid', 'softmax']): Score function to gave affinity scores
+
+    route_scale (float): Routing Scale factor
+
+    # rope
+    omega (int): Predefined constant for computing complex exponential values
+
+    # others
+    bias (bool): Do our model want to have bias term?
+
+    dropout (float): Regularization for randomly deactivating activation
+
+    max_seq_len (int): Maximum sequence lenght that model can handle
+    
+    """
+
+    n_embd: int = 192                
+    vocab_size: int = 128000        
+    n_layers: int = 5                 
+    block_size: int = 32             
+    batch_size: int = 16            
+    inter_dim: int = 768              
+
+    # MLA
+    q_lora_dim: int = 10                       
+    kv_lora_dim: int = 10                      
+    n_heads: int = 4                           
+    qk_nope_head_dim: int = 48             
+    qk_rope_head_dim: int = 24                 
+    v_head_dim: int = 48                       
+    n_dense_layers: int = 1                   
 
     # DeepSeekMOE
-    m: int = 4                                                # fine-grained expert segmentation
-    num_experts: int = 16 * m                                 # How many experts in MOE?
-    n_shared_experts: int = 2                                 # experts that are activated always
-    k: int = (2 * m) - n_shared_experts                       # How many routing active expert ?
-    n_routing_expert: int = num_experts - n_shared_experts    # num of experts that are activated sparsely
-    expert_dim: int = (4 * n_embd )// m                       # what's dim of expert?
+    m: int = 4                                               
+    num_experts: int = 16 * m                               
+    n_shared_experts: int = 2                                
+    n_routing_experts: int = num_experts - n_shared_experts  
+    n_activated_experts: int = (2 * m) - n_shared_experts    
+    experts_dim: int = (4 * n_embd ) // m                    
+    score_func: Literal['sigmoid', 'softmax'] = 'sigmoid'    
+    route_scale: float = 1.0
 
-    bias: bool = True
-    dropout: float = 0.2
+    # rope
+    omega: int = 10000.0
+
+    bias: bool = False
+    dropout: float = 0.1
+    max_seq_len: int = block_size
